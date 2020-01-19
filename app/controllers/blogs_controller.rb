f0701c3c2e4840956a09b900771431484a568f92
yuri0427a <yuri0427ita@gmail.com>
2020-01-19 21:43:18 +0900

    [Finish]4-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -15,7 +15,7 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
-end
+  end
 
   def edit
   	@blog = Blog.find(params[:id])
@@ -26,8 +26,14 @@ end
         blog.save
         redirect_to blogs_path
     end
+      def destroy
+      blog = Blog.find(params[:id])
+      blog.destroy
+      redirect_to blogs_path
+     end
      private
     def blog_params
       params.require(:blog).permit(:title, :category, :body)
-    end
+     end
+
 end
