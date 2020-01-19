3d058a5aab4fe4308b023d4d7e2e7dfd4f778870
yuri0427a <yuri0427ita@gmail.com>
2020-01-19 16:47:16 +0900

    [Finish]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,8 +6,19 @@ class BlogsController < ApplicationController
   end
 
   def new
+  	@blog = Blog.new
   end
 
   def edit
   end
+  def create
+        blog = Blog.new(blog_params)
+        blog.save
+        redirect_to blogs_path
+  end
+ 
+  private
+   def blog_params
+      params.require(:blog).permit(:title, :category, :body)
+   end
 end
